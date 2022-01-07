5e6b3bdbb98cd01ba1a9aa90517a0c79916d2685
jays-commit <engine123qazqwe>
2022-01-07 20:14:12 +0000

    win and lose print statements changed.

--- RPS_V1.py
+++ RPS_V1.py
@@ -15,6 +15,7 @@ while True:
 
     # player input loop
     while True:
+
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
         playerMove = input()
         if playerMove == 'q':
@@ -22,4 +23,52 @@ while True:
         if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
             break # Break out of the player input loop.
         print('Type one of r, p, s, or q.')
+
+
+        # Display what the player chose:
+    if playerMove == 'r':
+        print('ROCK versus...')
+    elif playerMove == 'p':
+        print('PAPER versus...')
+    elif playerMove == 's':
+        print('SCISSORS versus...')
+
+    # Display what the computer chose:
+    randomNumber = random.randint(1, 3)
+    if randomNumber == 1:
+        computerMove = 'r'
+        print('ROCK')
+    elif randomNumber == 2:
+        computerMove = 'p'
+        print('PAPER')
+    elif randomNumber == 3:
+        computerMove = 's'
+        print('SCISSORS')
+
+    if playerMove == computerMove:
+        print("Tie")
+        ties = ties +1
+
+    elif playerMove == "r" and computerMove == "s":
+        print("Rock smashes scissors! You win!")
+        wins = wins + 1
+
+    elif playerMove == "r" and computerMove == "p":
+        print("Paper covers rock! You lose.")
+        losses = losses + 1
+
+    elif playerMove == "s" and computerMove == "r":
+        print("Rock smashes scissors! You lose.")
+        losses = losses + 1
+
+    elif playerMove == "s" and computerMove == "p":
+        print("Scissors cuts paper! You win!")
+        wins = wins + 1
+    elif playerMove == "p" and computerMove == "s":
+        print("Scissors cuts paper! You lose.")
+        losses = losses + 1
+    elif playerMove == "p" and computerMove == "r":
+        print("Paper covers rock! You win!")
+        wins = wins + 1
+
 
