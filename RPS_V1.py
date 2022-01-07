359e59e1716e9ee3588c4cb88bd28ee037fa84e6
jays-commit <engine123qazqwe>
2022-01-07 12:21:25 +0000

    breaking down code

--- RPS_V1.py
+++ RPS_V1.py
@@ -7,9 +7,14 @@ wins = 0
 ties = 0
 losses = 0
 
-while True: # The main game loop.
+# The main loop for the game
+while True:
+
+    # Game score
     print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
-    while True: # The player input loop.
+
+    # player input loop
+    while True:
         print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
         playerMove = input()
         if playerMove == 'q':
@@ -17,47 +22,4 @@ while True: # The main game loop.
         if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
             break # Break out of the player input loop.
         print('Type one of r, p, s, or q.')
-    # Display what the player chose:
-    if playerMove == 'r':
-        print('ROCK versus...')
-    elif playerMove == 'p':
-        print('PAPER versus...')
-    elif playerMove == 's':
-        print('SCISSORS versus...')
-    # Display what the computer chose:
-    randomNumber = random.randint(1, 3)
-    if randomNumber == 1:
-        computerMove = 'r'
-        print('ROCK')
-    elif randomNumber == 2:
-        computerMove = 'p'
-        print('PAPER')
-    elif randomNumber == 3:
-        computerMove = 's'
-        print('SCISSORS')
 
-    if playerMove == computerMove:
-        print("Tie")
-        ties = ties +1
-
-    elif playerMove == "r" and computerMove == "s":
-        print("Win")
-        wins = wins + 1
-
-    elif playerMove == "r" and computerMove == "p":
-        print("Loss")
-        losses = losses + 1
-
-    elif playerMove == "s" and computerMove == "r":
-        print("Loss")
-        losses = losses + 1
-
-    elif playerMove == "s" and computerMove == "p":
-        print("Win")
-        wins = wins + 1
-    elif playerMove == "p" and computerMove == "s":
-        print("Loss")
-        losses = losses + 1
-    elif playerMove == "p" and computerMove == "r":
-        print("Win")
-        wins = wins + 1
