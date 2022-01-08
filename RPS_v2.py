bc83dacd0aa3060b3e0656c541314f0f4cc69f63
jays-commit <engine123qazqwe>
2022-01-08 15:31:47 +0000

    f string used to display player and computer actions main game loop created

--- RPS_v2.py
+++ RPS_v2.py
@@ -2,15 +2,55 @@ import random
 import sys
 import enum
 
+wins = 0
+losses = 0
+ties = 0
+
 print("Rock, Paper Scissors")
 
 while True:
 
     possible_moves = ["rock", "paper", "scissors"]
-    player_move = input('Enter your move: rock paper scissors or quit').lower()
+    print("Enter your move: rock paper scissors or quit ")
+    player_move = input().lower()
 
     if player_move == "quit":
         sys.exit()
 
     computer_move = random.choice(possible_moves)
-    print(f"You chose {player_move} and the computer chose {computer_move}")
+    print(f"{player_move.upper()} versus {computer_move.upper()}")
+    print(f"You chose {player_move} computer chose {computer_move}\n")
+
+    if player_move == computer_move:
+        print(f"player and computer both chose {player_move} it is a tie ")
+
+    elif player_move == "rock" and computer_move == "scissors":
+        print("Rock smashes scissors! You win!")
+        wins = wins + 1
+    elif player_move == "rock" and computer_move == "paper":
+        print("Paper covers rock! You lose.")
+        losses = losses + 1
+    elif player_move == "scissors" and computer_move == "rock":
+        print("Rock smashes scissors! You lose.")
+        losses = losses + 1
+    elif player_move == "scissors" and computer_move == "paper":
+        print("Scissors cuts paper! You win!")
+        wins = wins + 1
+    elif player_move == "paper" and computer_move == "scissors":
+        print("Scissors cuts paper! You lose.")
+        losses = losses + 1
+    elif player_move == "paper" and computer_move == "rock":
+        print("Paper covers rock! You win!")
+        wins = wins + 1
+    while True:
+
+        game_over = input("Play again? (y/n):\n")
+        if game_over.lower() == "y":
+
+            # Game score
+            print("%s Wins, %s Losses, %s Ties\n" % (wins, losses, ties))
+            break
+        elif game_over.lower() == "n":
+            sys.exit()
+        else:
+            print("please enter y/n\n")
